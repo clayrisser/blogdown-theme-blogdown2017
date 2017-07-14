2c9f7395582db243c68c6a9052362df134a117de
Jam Risser <jam@jamrizzi.com>
2017-07-14 16:58:53 -0500

    Moved to gulp build system

--- tasks/copy.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-
-export default async function copy() {
-  await new Promise((resolve, reject) => {
-    gulp.src('./bower.json')
-      .pipe(gulp.dest('./dist/'))
-      .on('error', reject).on('end', resolve);
-  });
-}
